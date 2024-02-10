81ebcfed515be9cc7249bf9710daec071e92dfb2
sagarlamani <sagarlamani23@gmail.com>
2024-02-10 12:29:53 +0530

    minor changes

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as file:
 
 setuptools.setup(
 	name = 'preprocess_sagarlamani',
-	version = '0.0.1',
+	version = '0.0.2',
 	author = 'Sagar Lamani',
 	author_email = 'sagarlamani23@gmail.com',
 	description = 'This is a text preprocessing package',
